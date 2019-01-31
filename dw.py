f8fdbebdd1b595822259cf82f9efad4d68c0f0fc
poxmel2019 <46764191+poxmel2019@users.noreply.github.com>
2019-01-31 20:27:22 +0600

    Add files via upload

--- dw.py
+++ dw.py
@@ -37,23 +37,28 @@ def ff():
     print('This is an array')
     print(mas)
 
-    #while True:
-    path = input('Point path:\n')
-    try:
-        with open (path) as file:
-            for x in file:
-                mas += [int(x)]
-            file.close()
-    except FileNotFoundError:
-        print('Error')
-    print("This is an array: {0}".format(mas))
-    try:    
-        print("This is a maximum: {0}".format(max(mas))) # handy
-        print("This is a minimum: {0}".format(min(mas))) # handy
-        print("This is a mean: {0}".format(np.mean(mas))) # handy
-        print("This is a median: {0}".format(np.mean(mas))) # handy
-        print("This is a standard deviation: {0}".format(np.std(mas))) # handy
-        print("This is a dispersion: {0}".format(np.var(mas))) # handy
-    except ValueError:
-        print('Empty array')
+    while True:
+        path = input('Point path:\n')
+        try:
+            with open (path) as file:
+                for x in file:
+                    mas += [int(x)]
+                file.close()
+        except FileNotFoundError:
+            if path == 'q': break
+            else: print('Error')
+        except OSError:
+            print('There is no the path')
+
+        print("This is an array: {0}".format(mas))
+
+        try:    
+            print("This is a maximum: {0}".format(max(mas))) # handy
+            print("This is a minimum: {0}".format(min(mas))) # handy
+            print("This is a mean: {0}".format(np.mean(mas))) # handy
+            print("This is a median: {0}".format(np.mean(mas))) # handy
+            print("This is a standard deviation: {0}".format(np.std(mas))) # handy
+            print("This is a dispersion: {0}".format(np.var(mas))) # handy
+        except ValueError:
+            print('Empty array')
     
